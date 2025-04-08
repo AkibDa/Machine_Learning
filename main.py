a85913e282b65fa504eea71ef35611706f5411c0
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-04-08 21:07:01 +0530

    Fifth lesson

--- main.py
+++ main.py
@@ -29,3 +29,16 @@ melbourne_model = DecisionTreeRegressor()
 melbourne_model.fit(train_X, train_y)
 val_predictions = melbourne_model.predict(val_X)
 print(mean_absolute_error(val_y, val_predictions))
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+  
+train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
+
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
