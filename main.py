af921c5a39a2a750c3a6cf4f8d4f877c0c08b1f4
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-04-08 21:01:45 +0530

    Build First Model

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 melbourne_file_path = './melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
 print(melbourne_data.describe())
 
 melbourne_new_data = melbourne_data.dropna(axis=0)
-y = melbourne_data.Price
+y = melbourne_new_data.Price
 melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
 X = melbourne_new_data[melbourne_features]
 print(X.describe())
 print(X.head())
+
+melbourne_model = DecisionTreeRegressor(random_state=1)
+melbourne_model.fit(X, y)
+print("Making predictions for the following 5 houses:")
+print(X.head())
+print("The predictions are")
+print(melbourne_model.predict(X.head()))
