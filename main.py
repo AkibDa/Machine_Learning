80768059cbcf62a7c145fd6f97a952a8ec9b12f2
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-04-08 20:56:05 +0530

    Second lesson

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import pandas as pd
 melbourne_file_path = './melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
 print(melbourne_data.describe())
+
+melbourne_new_data = melbourne_data.dropna(axis=0)
+y = melbourne_data.Price
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+X = melbourne_new_data[melbourne_features]
+print(X.describe())
