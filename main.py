84384415ce11531da03d8b28743d9b27e2b74fa4
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-04-08 21:10:15 +0530

    Completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 
 melbourne_file_path = './melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
@@ -42,3 +43,8 @@ train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
 for max_leaf_nodes in [5, 50, 500, 5000]:
     my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
     print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+    
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
