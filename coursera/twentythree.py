64ac87154746adc7c858de27c96f5d6ac8e794f9
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-15 23:19:52 +0530

    Loading the dataset

--- coursera/twentythree.py
+++ coursera/twentythree.py
@@ -2,3 +2,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.preprocessing import StandardScaler
+
+data = {'AnnualIncome': [
+        15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5,
+        20, 20.5, 21, 21.5, 22, 22.5, 23, 23.5, 24, 24.5,
+        25, 25.5, 26, 26.5, 27, 27.5, 28, 28.5, 29, 29.5,
+        30, 30.5, 31, 31.5, 32, 32.5, 33, 33.5, 34, 34.5,
+        35,   # Normal points
+        80, 85, 90  # Outliers
+    ],
+    'SpendingScore': [
+        39, 42, 45, 48, 51, 54, 57, 60, 63, 66,
+        69, 72, 75, 78, 81, 84, 87, 90, 93, 96,
+        6, 9, 12, 15, 18, 21, 24, 27, 30, 33,
+        5, 8, 11, 14, 17, 20, 23, 26, 29, 32,
+        56,   # Normal points
+        2, 3, 100  # Outliers
+    ]}
+
+df = pd.DataFrame(data)
+
+# Display the first few rows of the dataset
+print(df.head())
