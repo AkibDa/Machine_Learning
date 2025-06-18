683aa10c2352a251b2126694f273911419d35aee
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-18 16:03:26 +0530

    splitting the dataset into training and testing data

--- github-repo/day_1.py
+++ github-repo/day_1.py
@@ -18,3 +18,6 @@ onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder()
 Y =  labelencoder_Y.fit_transform(Y)
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
